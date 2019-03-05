deeba188fcfb1888675c470f314904c4bb389b0d
kyu <kyukyu0901@gmail.com>
2019-03-05 19:42:54 +0900

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi!!");
+const express = require('express')
+//어플리케이션 만들기. express application
+const app = express()
+
+const PORT = 4000;
+
+//listening이 시작될 때 호출됨.
+function handleListening(){
+    console.log('Listening on: http://localhost:${PORT}');
+}
+
+//application에게 PORT번호를 listen하게 한다.
+app.listen(PORT, handleListening);
